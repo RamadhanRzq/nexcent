2c3bccd5fadae46efcbd055c58742be08044990e
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2023-11-17 15:43:12 +0700

    Add Component

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,4 +1,5 @@
 import ic1 from "../assets/ic1.png";
+import ilus2 from "../assets/ilus2.png";
 
 function Feature() {
   return (
@@ -48,6 +49,23 @@ function Feature() {
             <br />
             membership renewals and payments
           </p>
+        </div>
+      </div>
+      <div className="flex mt-36 mx-72">
+        <img src={ilus2} alt="logoilus" />
+        <div className="flex-col">
+          <p className="text-Headline2 font-semibold">
+            The unseen of spending three <br /> years at Pixelgrade
+          </p>
+          <p className="mt-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+            amet justo ipsum. Sed accumsan quam vitae est varius fringilla.
+            Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
+            tristique iaculis. Nullam pulvinar sit amet risus pretium auctor.
+            Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec
+            elementum pulvinar odio.
+          </p>
+          <button className='bg-primary hover:bg-shade1 py-3 px-5 rounded-lg text-white mt-8'>Learn More</button>
         </div>
       </div>
     </div>
